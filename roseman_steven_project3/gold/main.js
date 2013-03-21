39085ef40b8d61355381a5eb40d9c08413366755
Steven Roseman <stevierose@fullsail.edu>
2013-03-21 11:37:25 -0400

    added gold folder w/ validation working

---
+++ roseman_steven_project3/gold/main.js
@@ -0,0 +1,43 @@
+$('home').on('pageinit', function(){
+	
+});
+
+$('aboutUs2').on('pageinit', function(){
+	
+});
+
+$('news').on('pageinit', function(){
+	
+});
+
+$('#addWed').on('pageinit', function(){
+
+		var myForm = $('#wedForm');
+                var errorsLink = $('#errorsLink');
+		    myForm.validate();
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
