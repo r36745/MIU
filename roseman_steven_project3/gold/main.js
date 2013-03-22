ad7e76230cf93cf0e954571470a1e9ee96528032
Steven Roseman <stevierose@fullsail.edu>
2013-03-21 22:55:59 -0400

    updated js local storage working

--- roseman_steven_project3/gold/main.js
+++ roseman_steven_project3/gold/main.js
@@ -1,3 +1,18 @@
+// Steven Roseman
+// MIU
+// Term 1303
+// Project 3
+
+
+function  getEle(x) {
+        var theElement = document.getElementById(x);
+        return theElement;
+    }
+    function createEle(y) {
+        var createElement = document.createElement(y);
+        return createElement;
+    }
+
 $('home').on('pageinit', function(){
 	
 });
@@ -14,7 +29,26 @@ $('#addWed').on('pageinit', function(){
 
 		var myForm = $('#wedForm');
                 var errorsLink = $('#errorsLink');
-		    myForm.validate();
+		    myForm.validate({
+                        invalidHandler: function(form, validator) {
+                         errorsLink.click();
+                        var html = '';
+                        for(var key in validator.submitted){
+                        var label = $('label[for^="'+ key +'"]').not('[generated]');
+                        var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+                        var fieldName = legend.length ? legend.text() : label.text();
+                        html += '<li>' + fieldName + '</li>';
+
+                        };
+                        $("#recordErrors ul").html(html);
+                        },
+                        submitHandler: function() {
+                            
+                            var data = myForm.serializeArray();
+                            storeData(data);
+                            
+                        }
+                    });
 	
 	//any other code needed for addItem page goes here
 	
@@ -30,8 +64,21 @@ var getData = function(){
 
 };
 
-var storeData = function(data){
-	
+
+
+function storeData(data){
+    var id                      = Math.floor(Math.random()*1000000001);
+    
+    var item                    ={};
+        item.group              =["Group", getEle('wedGrp').value];
+        item.fname              =["First Name", getEle('fname').value];
+        item.lname              =["Last Name", getEle('lname').value];
+        item.email              =["Email", getEle('email').value];
+        item.dinnerOpt          =["Dinner Choice", dinnerValue];
+        item.picture            =["Picture", getEle('funPic1').value];
+        
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("contact saved");
 }; 
 
 var	deleteItem = function (){
@@ -41,3 +88,12 @@ var	deleteItem = function (){
 var clearLocal = function(){
 
 };
+
+
+var dinnerValue;
+
+
+
+
+
+
