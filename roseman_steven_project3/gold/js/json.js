7bb74be34fa24bc359721428217a83e50a3fc704
Steven Roseman <stevierose@fullsail.edu>
2013-03-21 20:48:42 -0400

    added bronze file. Updated gold app. Still having issues w/ local storage.

---
+++ roseman_steven_project3/gold/js/json.js
@@ -0,0 +1,229 @@
+/*
+ Steven Roseman
+ MIU
+ Term 1303
+ Project 1
+ */
+
+var json = {
+    "Guest1": {
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Steven"],
+        "lname": ["Last Name:", "Roseman"],
+        "email": ["Email:", "steven.roseman@gmail.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "Proud of you"]
+    },
+    "Guest2" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Brandy"],
+        "lname": ["Last Name:", "Wilks"],
+        "email": ["Email:", "brandyw@gmail.com"],
+        "interest": ["Interest:", "2"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-22-03"],
+        "comments": ["Comments:", "Wouldnt miss it"]
+    },
+    "Guest3" :{
+        "group": ["Group:", "Friend"],
+        "fname": ["First Name:", "Russell"],
+        "lname": ["Last Name:", "Lyles"],
+        "email": ["Email:", "russell@glaw.com"],
+        "interest": ["Interest:", "7"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "No"],
+        "date": ["Date:", "2013-01-01"],
+        "comments": ["Comments:", "You fool"]
+    },
+    "Guest4" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "El"],
+        "lname": ["Last Name:", "Lucas"],
+        "email": ["Email:", "lucase@ymail.com"],
+        "interest": ["Interest:", "5"],
+        "dinnerOpt": ["Dinner Choice:", "Veggie Platter"],
+        "picture": ["Picture:", ""],
+        "date": ["Date:", "2013-14-05"],
+        "comments": ["Comments:", "I dont want to take a pic"]
+    },
+    "Guest5" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Stephanie"],
+        "lname": ["Last Name:", "Roseman"],
+        "email": ["Email:", "stephanie.roseman@gmail.com"],
+        "interest": ["Interest:", "3"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-04"],
+        "comments": ["Comments:", ""]
+    },
+    "Guest6" :{
+        "group": ["Group:", "Friend"],
+        "fname": ["First Name:", "Randy"],
+        "lname": ["Last Name:", "Griffin"],
+        "email": ["Email:", "randyg@gmail.com"],
+        "interest": ["Interest:", "8"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-05-02"],
+        "comments": ["Comments:", "Hope there's a free bar"]
+    },
+    "Guest7" :{
+        "group": ["Group:", "Friend"],
+        "fname": ["First Name:", "Toi"],
+        "lname": ["Last Name:", "James"],
+        "email": ["Email:", "toij@breckers.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Veggie Platter"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-12-06"],
+        "comments": ["Comments:", "So proud of you girl"]
+    },
+    "Guest8" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Kim"],
+        "lname": ["Last Name:", "Jones"],
+        "email": ["Email:", "kj@meiser.com"],
+        "interest": ["Interest:", "9"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "No"],
+        "date": ["Date:", "2012-14-12"],
+        "comments": ["Comments:", "Nice meal options"]
+    },
+    "Guest9" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Frank"],
+        "lname": ["Last Name:", "Coombs"],
+        "email": ["Email:", "coombsf@federal.com"],
+        "interest": ["Interest:", "0"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-22-04"],
+        "comments": ["Comments:", "you lucky i like you, I hate weddings"]
+    },
+    "Guest10" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Johnsie"],
+        "lname": ["Last Name:", "Lucas"],
+        "email": ["Email:", "johnsie@gmail.com"],
+        "interest": ["Interest:", "1"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "you know I cant dance anymore"]
+    },
+    "Guest11" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Monica"],
+        "lname": ["Last Name:", "Waters"],
+        "email": ["Email:", "jollyjoe@gmail.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Veggie Platter"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "Weddings make me cry"]
+    },
+    "Guest12" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Brandon"],
+        "lname": ["Last Name:", "Williams"],
+        "email": ["Email:", "henny@gmail.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "I could have catered this event. But I'll be there"]
+    },
+    "Guest13" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Mysi"],
+        "lname": ["Last Name:", "Lee"],
+        "email": ["Email:", "mysi@pfpc.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", "No"],
+        "date": ["Date:", "2013-14-04"],
+        "comments": ["Comments:", "I hate your fiancee girl, bu I love you!"]
+    },
+    "Guest14" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Bea"],
+        "lname": ["Last Name:", "Ceja"],
+        "email": ["Email:", "ceja@gov.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Veggie Platter"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-18-02"],
+        "comments": ["Comments:", "I'm crying"]
+    },
+    "Guest15" :{
+        "group": ["Group:", "Attendee"],
+        "fname": ["First Name:", "Bruce"],
+        "lname": ["Last Name:", "Roseman"],
+        "email": ["Email:", "bruce@gmail.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "whose paying for my tux"]
+    },
+    "Guest16" :{
+        "group": ["Group:", "Friend"],
+        "fname": ["First Name:", "Lisa"],
+        "lname": ["Last Name:", "Rose"],
+        "email": ["Email:", "lrose@gmail.com"],
+        "interest": ["Interest:", "2"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", ""],
+        "comments": ["Comments:", ""]
+    },
+    "Guest17" :{
+        "group": ["Group:", "Friend"],
+        "fname": ["First Name:", "Anthony"],
+        "lname": ["Last Name:", "Jiles"],
+        "email": ["Email:", "jiles@gmail.com"],
+        "interest": ["Interest:", "1"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", ""],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", ""]
+    },
+    "Guest18" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Anthony"],
+        "lname": ["Last Name:", "Jenkins"],
+        "email": ["Email:", "aj@gmail.com"],
+        "interest": ["Interest:", "6"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", ""]
+    },
+    "Guest19" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Bernie"],
+        "lname": ["Last Name:", "Jenkins"],
+        "email": ["Email:", "jenkinbern@bwj.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Baked Chicken"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "I'm not bringing a gift"]
+    },
+    "Guest20" :{
+        "group": ["Group:", "Family"],
+        "fname": ["First Name:", "Jean"],
+        "lname": ["Last Name:", "Ray"],
+        "email": ["Email:", "mil@gmail.com"],
+        "interest": ["Interest:", "10"],
+        "dinnerOpt": ["Dinner Choice:", "Shrimp Alfredo"],
+        "picture": ["Picture:", "Yes"],
+        "date": ["Date:", "2013-14-02"],
+        "comments": ["Comments:", "What a happy occasion"]
+    }
+}
